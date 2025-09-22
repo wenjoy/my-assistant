24c7bcf82f5896f98f27bce74b1cf8aab3eab5ff
wenjoy <laojiaodaoniu@126.com>
2025-09-22 11:27:54 +0800

    wip: try to fix 446 error

---
+++ req.sh
@@ -0,0 +1,3 @@
+curl -vv 'https://www.cninfo.com.cn/new/hisAnnouncement/query' \
+  -H 'Content-Type: application/x-www-form-urlencoded' \
+  --data-raw 'stock=601916%2C9900007207'
--- src/main.rs
+++ src/main.rs
@@ -3,33 +3,32 @@ use std::collections::HashMap;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // let url = "https://httpbin.org/ip";
-    let url = "https://www.cninfo.com.cn/new/hisAnnouncement/query";
-    // let resp = reqwest::get(url).await?.json::<HashMap<String, String>>().await?;
+    // let url = "https://httpbin.org/anything";
+    let url = "http://localhost:8080";
+    // let url = "https://www.cninfo.com.cn/new/hisAnnouncement/query";
+
     let client = reqwest::Client::new();
+
     let mut map = HashMap::new();
     map.insert("stock", "601916,9900007207");
-    map.insert("tabName", "fulltext");
-    map.insert("pageSize", "30");
-    map.insert("pageNumber", "1");
-    map.insert("plate", "sh");
-    map.insert("plate", "sh");
-    map.insert("isHLtitle", "true");
+    // map.insert("tabName", "fulltext");
+    // map.insert("pageSize", "30");
+    // map.insert("pageNumber", "1");
+    // map.insert("plate", "sh");
+    // map.insert("plate", "sh");
+    // map.insert("isHLtitle", "true");
 
     // let forms:Vec<_> = map.into_iter().collect();
-    let request = client
-        .post(url)
-        .form(&map);
+    let request = client.post(url).form(&map);
 
-    let resp = request
-    .send()
-    .await?;
+    // let resp = reqwest::get(url).await?.json::<HashMap<String, String>>().await?;
+    let resp = request.send().await?;
 
     let status = resp.status();
     println!("status: {status}");
 
-    let result = resp
-        .text()
-        .await?;
+    // let result = resp.json::<HashMap<String, String>>().await?;
+    let result = resp.text().await?;
 
     println!("{result:#?}");
     Ok(())
