49b5046254c0a11fd708875339e2173a4c2c3881
wenjoy <laojiaodaoniu@126.com>
2025-09-24 10:24:41 +0800

    refact: extract fetch logic to lib

---
+++ src/lib.rs
@@ -0,0 +1,38 @@
+use serde::Deserialize;
+use std::{collections::HashMap, error::Error};
+
+#[derive(Deserialize, Debug)]
+struct Announcement {
+    announcementTitle: String,
+    announcementTime: i64,
+    announcementId: String,
+    adjunctType: String,
+    adjunctUrl: String,
+}
+#[derive(Deserialize, Debug)]
+pub struct Response {
+    announcements: Vec<Announcement>,
+}
+
+pub async fn fetch() -> Result<Response, Box<dyn Error>> {
+    let url = "https://www.cninfo.com.cn/new/hisAnnouncement/query";
+
+    let client = reqwest::Client::builder()
+        .user_agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36")
+        .build()?;
+
+    let mut map = HashMap::new();
+    map.insert("stock", "601916,9900007207");
+
+    let request = client.post(url).form(&map);
+
+    let resp = request.send().await?;
+
+    let status = resp.status();
+    println!("status: {status}");
+
+    let result: Response = resp.json().await?;
+
+    println!("{result:#?}");
+    Ok(result)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,7 @@
-use serde::Deserialize;
-use std::collections::HashMap;
+use my_assitant::fetch;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let url = "https://www.cninfo.com.cn/new/hisAnnouncement/query";
-
-    let client = reqwest::Client::builder()
-        .user_agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36")
-        .build()?;
-
-    let mut map = HashMap::new();
-    map.insert("stock", "601916,9900007207");
-
-    let request = client.post(url).form(&map);
-
-    let resp = request.send().await?;
-
-    let status = resp.status();
-    println!("status: {status}");
-
-    #[derive(Deserialize, Debug)]
-    struct Announcement {
-        announcementTitle: String,
-        announcementTime: i64,
-        announcementId: String,
-        adjunctType: String,
-        adjunctUrl: String,
-    }
-    #[derive(Deserialize, Debug)]
-    struct Response {
-        announcements: Vec<Announcement>,
-    }
-
-    let result: Response = resp.json().await?;
-
-    println!("{result:#?}");
+    fetch().await?;
     Ok(())
 }
