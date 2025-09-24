6b5075c8838b8274b192aeb0dde2794e5a0018df
wenjoy <laojiaodaoniu@126.com>
2025-09-24 21:33:24 +0800

    fix: talbe not exist error (using two saparate connection)

--- src/db.rs
+++ src/db.rs
@@ -1,10 +1,27 @@
 use sqlx::{Connection, Error, SqliteConnection};
 
-pub async fn create_shema() -> Result<(), Error> {
-    let mut conn = SqliteConnection::connect("sqlite::memory:").await?;
+pub async fn create_shema(conn: &mut SqliteConnection) -> Result<(), Error> {
+    let res = sqlx::query(
+        "
+        CREATE TABLE IF NOT EXISTS announcements (
+        announcement_title TEXT,
+        announcement_time INTEGER,
+        announcement_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        adjunct_type TEXT,
+        adjunct_url TEXT
+        );
+        ",
+    )
+    .execute(conn)
+    .await?;
+    println!("create: {res:?}");
+    Ok(())
+}
+
+pub async fn query_data(conn: &mut SqliteConnection) -> Result<(), Error> {
     let res = sqlx::query("select * from announcements")
-        .execute(&mut conn)
+        .execute(conn)
         .await?;
-    println!("res: {res:?}");
+    println!("query: {res:?}");
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
-use my_assitant::{db::create_shema, fetch};
+use my_assitant::{
+    db::{create_shema, query_data},
+    fetch,
+};
+use sqlx::{Connection, SqliteConnection};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // fetch().await?;
-    create_shema().await?;
+    let mut conn = SqliteConnection::connect("sqlite::memory:").await?;
+    create_shema(&mut conn).await?;
+    query_data(&mut conn).await?;
     Ok(())
 }
