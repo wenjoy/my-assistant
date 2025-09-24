f3749870db2cd7661aeae1b18b4900423403965f
wenjoy <laojiaodaoniu@126.com>
2025-09-24 21:19:19 +0800

    feat: integrate with sqlite

--- src/db.rs
+++ src/db.rs
@@ -1,6 +1,10 @@
-use sqlx::{Connection, SqliteConnection};
+use sqlx::{Connection, Error, SqliteConnection};
 
-pub async fn create_shema(db_url: &str) {
-    let conn = SqliteConnection::connect("sqlite:memory:").await;
-    sqlx::query("select * from announcements").execute(&mut conn);
+pub async fn create_shema() -> Result<(), Error> {
+    let mut conn = SqliteConnection::connect("sqlite::memory:").await?;
+    let res = sqlx::query("select * from announcements")
+        .execute(&mut conn)
+        .await?;
+    println!("res: {res:?}");
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ use my_assitant::{db::create_shema, fetch};
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // fetch().await?;
-    create_shema();
+    create_shema().await?;
     Ok(())
 }
