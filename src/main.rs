7a974c75eade5049ca9798473118d1388354f98c
wenjoy <laojiaodaoniu@126.com>
2025-09-29 05:03:43 +0800

    feat: read pdf example

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use my_assitant::{
     db::{create_shema, query_data},
-    pdf::fetch_pdf,
+    pdf::{fetch_pdf, read_pdf},
 };
 use sqlx::{Connection, SqliteConnection};
 
@@ -12,11 +12,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // for item in result.announcements {
     //     insert_data(&mut conn, item).await?;
     // }
-    let urls = query_data(&mut conn).await?;
-    for url in urls {
-        println!("url is {url}");
-        fetch_pdf(&url).await;
-        break;
-    }
+    // let urls = query_data(&mut conn).await?;
+    // for url in urls {
+    //     println!("url is {url}");
+    //     fetch_pdf(&url).await;
+    //     break;
+    // }
+    read_pdf();
     Ok(())
 }
--- src/pdf.rs
+++ src/pdf.rs
@@ -1,4 +1,18 @@
-pub fn read_pdf() {}
+use oxidize_pdf::PdfReader;
+
+pub fn read_pdf() {
+    // TODO: use either pdf_extract or oxidize_pdf
+    let bytes = std::fs::read("/Users/wenjoy/Downloads/demo.pdf").unwrap();
+    let content = pdf_extract::extract_text_from_mem(&bytes).unwrap();
+    println!("content is {content:?}, len: {}", bytes.len());
+    // let mut reader = PdfReader::open("/Users/wenjoy/Downloads/demo.pdf").unwrap();
+    // let document = reader.into_document();
+    // let text = document.extract_text();
+
+    // for (page_num, page_text) in text.iter().enumerate() {
+    //     println!("Page {}: {:?}", page_num + 1, page_text);
+    // }
+}
 pub async fn fetch_pdf(url: &str) -> Result<(), reqwest::Error> {
     let host = "https://static.cninfo.com.cn/";
     let url = String::from(host) + url;
