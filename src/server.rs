007ca65647da2cb2b8a3308e8617dd873c39458f
wenjoy <laojiaodaoniu@126.com>
2025-10-15 11:21:18 +0800

    wip

--- src/server.rs
+++ src/server.rs
@@ -3,16 +3,26 @@ use std::{
     net::{TcpListener, TcpStream},
 };
 
-pub async fn http_server() {
+enum HTTP_METHOD {
+    GET,
+    POST,
+}
+struct Router {
+    method: HTTP_METHOD,
+    path: String,
+    handler: Box<dyn Fn() -> ()>,
+}
+
+pub async fn http_server(router: Router) {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+        handle_connection(stream, router);
     }
 }
 
-fn handle_connection(mut stream: TcpStream) {
+fn handle_connection(mut stream: TcpStream, router: Router) {
     let buffer = BufReader::new(&stream);
     // let http_request: Vec<_> = buffer
     //     .lines()
@@ -21,7 +31,9 @@ fn handle_connection(mut stream: TcpStream) {
     //     .collect();
     // println!("Request {http_request:?}");
     let request_line = buffer.lines().next().unwrap().unwrap();
-    let (status_lien, content) = if request_line == "GET / HTTP/1.1" {
+    let request_line: Vec<&str> = request_line.split(" ").collect();
+    let [method, path, _] = request_line[..];
+    let (status_lien, content) = if method == router.method && path == router.path {
         ("HTTP/1.1 200 OK\r\n\r\n", "simple string")
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "Not found")
