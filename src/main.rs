cf7e66d5b2ce1d24a778209ebf1b73e67f9acade
wenjoy <laojiaodaoniu@126.com>
2025-10-22 19:59:22 +0800

    fix: sqlite cannot connect issue

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use my_assitant::{
     db::query_all_data,
     server::{HttpMethod, Router, http_server},
 };
+use sqlx::Row;
 use sqlx::{Connection, SqliteConnection};
 
 #[tokio::main]
@@ -14,10 +15,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         path: "/announcements".to_string(),
         handler: Box::new(|| {
             Box::pin(async {
-                println!("hello");
-                // let mut conn = SqliteConnection::connect("sqlite::data.db").await?;
-                // let data = query_all_data(&mut conn).await?;
-                // Ok(())
+                let mut conn = SqliteConnection::connect("sqlite:data.db").await.unwrap();
+                let data = query_all_data(&mut conn).await.unwrap();
+                println!(
+                    "data is {}",
+                    data[0].try_get::<i64, _>("announcement_time").unwrap()
+                );
+                ()
             })
         }),
     };
