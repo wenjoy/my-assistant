8756c15629f1bc887c478556393d933be6fafa76
wenjoy <laojiaodaoniu@126.com>
2025-10-22 09:10:19 +0800

    feat: complete customized router function with async closure supported

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use my_assitant::{
     crawl,
+    db::query_all_data,
     server::{HttpMethod, Router, http_server},
 };
+use sqlx::{Connection, SqliteConnection};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -11,8 +13,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         method: HttpMethod::GET,
         path: "/announcements".to_string(),
         handler: Box::new(|| {
-            println!("handle router");
-            ()
+            Box::pin(async {
+                println!("hello");
+                // let mut conn = SqliteConnection::connect("sqlite::data.db").await?;
+                // let data = query_all_data(&mut conn).await?;
+                // Ok(())
+            })
         }),
     };
     http_server(router).await;
--- src/server.rs
+++ src/server.rs
@@ -1,6 +1,8 @@
 use std::{
+    future::Future,
     io::{BufRead, BufReader, Write},
     net::{TcpListener, TcpStream},
+    pin::Pin,
 };
 
 use strum_macros::{AsRefStr, EnumString};
@@ -13,22 +15,25 @@ pub enum HttpMethod {
     POST,
 }
 
+type BoxFuture = Pin<Box<dyn Future<Output = ()>>>;
+// pub type BoxFuture<'a> = Pin<Box<dyn Future<Output = ()> + Send + 'a>>;
+
 pub struct Router {
     pub method: HttpMethod,
     pub path: String,
-    pub handler: Box<dyn Fn() -> ()>,
+    pub handler: Box<dyn FnMut() -> BoxFuture>,
 }
 
-pub async fn http_server(router: Router) {
+pub async fn http_server(mut router: Router) {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream, &router);
+        handle_connection(stream, &mut router).await;
     }
 }
 
-fn handle_connection(mut stream: TcpStream, router: &Router) {
+async fn handle_connection(mut stream: TcpStream, router: &mut Router) {
     let buffer = BufReader::new(&stream);
     // let http_request: Vec<_> = buffer
     //     .lines()
@@ -40,7 +45,7 @@ fn handle_connection(mut stream: TcpStream, router: &Router) {
     let request_line: Vec<&str> = request_line.split(" ").collect();
     let [method, path] = [request_line[0], request_line[1]];
     let (status_lien, content) = if method == router.method.as_ref() && path == router.path {
-        (*router.handler)();
+        (router.handler)().await;
         ("HTTP/1.1 200 OK\r\n\r\n", "simple string")
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "Not found")
