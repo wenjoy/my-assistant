db44dc074c261286217cab1fc580080d6853d098
wenjoy <laojiaodaoniu@126.com>
2025-10-29 18:26:13 +0800

    fix: use custom deserialize to solve json data type uncompatible with database issue

--- src/model.rs
+++ src/model.rs
@@ -1,10 +1,20 @@
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Deserializer, Serialize};
 use sqlx::prelude::FromRow;
+use std::str::FromStr;
+
+fn deserialize_string_to_integer<'d, D: Deserializer<'d>>(
+    deserializer: D,
+) -> Result<i64, D::Error> {
+    let s: &str = Deserialize::deserialize(deserializer)?;
+    let parse_result = i64::from_str(s);
+    parse_result.map_err(serde::de::Error::custom)
+}
 
 #[derive(Deserialize, Serialize, Debug, FromRow)]
 pub struct Announcement {
+    #[serde(deserialize_with = "deserialize_string_to_integer")]
     #[sqlx(rename = "announcement_id")]
-    pub announcementId: String,
+    pub announcementId: i64,
     #[sqlx(rename = "announcement_title")]
     pub announcementTitle: String,
     #[sqlx(rename = "announcement_time")]
